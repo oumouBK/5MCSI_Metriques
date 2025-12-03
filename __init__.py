f21edb98006e3abe4c77599c0bb63a0cf586c006
oumouBK <Oumoubaka21@gmail.com>
2025-12-03 12:38:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
+from datetime import datetime
+from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
+    return render_template('hello.html')
+  
+@app.route('/extract-minutes/<date_string>/')
+def extract_minutes(date_string):
+    # date_string : "2024-02-11T11:57:27Z"
+    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+    minutes = date_object.minute
+    return jsonify({'minutes': minutes})
 
-@app.route('/exercices/')
-def exercices():
-    return render_template('exercices.html')
-                                                                                                               
+  
 if __name__ == "__main__":
   app.run(debug=True)
